ea46b858c47d66bd92d82a24461c762938b8bbaf
grahamison <grahamison@users.noreply.github.com>
2021-02-19 15:51:23 +0000

    Add options for x axis label and tick marks

--- R/theme_gss.R
+++ R/theme_gss.R
@@ -10,6 +10,9 @@
 #' @param base_line_size The base size for lines controlled by the theme
 #'   function (i.e. non-data lines).
 #' @param flipped A logical value indicating whether the chart has flipped axes.
+#' @param ticks A logical value indicating whether to add tick marks to the x
+#'   axis. Only adds ticks if `flipped = FALSE`.
+#' @param xlabel A logical value indicating whether to show the x axis label.
 #' @importFrom ggplot2 theme
 #' @importFrom ggplot2 element_text
 #' @importFrom ggplot2 element_line
@@ -28,7 +31,7 @@
 #'        subtitle = "England and Wales, Q1 2011 - Q2 2020",
 #'        caption = "Source: Family court statistics quarterly, April to June 2020 (table 1)") +
 #'   theme_gss(base_size = 18)
-theme_gss <- function(base_size = 20, base_line_size = 0.5, flipped = FALSE){
+theme_gss <- function(base_size = 20, base_line_size = 0.5, flipped = FALSE, ticks = FALSE, xlabel = FALSE){
 
   # Base theme object
   base_elements <- theme_grey(base_size = base_size, base_line_size = base_line_size) +
@@ -36,6 +39,7 @@ theme_gss <- function(base_size = 20, base_line_size = 0.5, flipped = FALSE){
           panel.grid = element_blank(),
           axis.ticks = element_blank(),
           axis.ticks.length = unit(0.5, "lines"),
+          axis.title = element_blank(),
           panel.background = element_blank(),
           legend.title = element_blank(),
           legend.key = element_blank(),
@@ -50,25 +54,43 @@ theme_gss <- function(base_size = 20, base_line_size = 0.5, flipped = FALSE){
   # To add for regular orientation
   regular_elements <- theme(
     panel.grid.major.y = element_line(),
-    axis.line.x = element_line(),
-    axis.title.y = element_blank(),
-    axis.title.x = element_text(margin = margin(t = 10, b = 10))
+    axis.line.x = element_line()
   )
 
   # To add for flipped orientation
   flipped_elements <- theme(
     panel.grid.major.x = element_line(),
     axis.line.y = element_line(),
-    axis.title.y = element_blank(),
-    axis.text.y = element_text(margin = margin(r = 10)),
-    axis.text.x = element_text(margin = margin(b = 10))
+    axis.title = element_blank(),
+    axis.text.y = element_text(margin = margin(r = 10))
   )
 
-  if (flipped == FALSE) {
-    base_elements +
+  # To add x axis ticks
+  xticks_element <- theme(
+    axis.ticks.x = element_line()
+  )
+
+  # To add x axis label
+  xlabel_element <- theme(
+    axis.title.x = element_text(margin = margin(t = 10))
+  )
+
+  # Build final theme
+  if (!flipped) {
+    t <- base_elements +
       regular_elements
   } else {
-    base_elements +
+    t <- base_elements +
       flipped_elements
   }
+
+  if (!flipped && ticks)
+    t <- t +
+      xticks_element
+
+  if (xlabel)
+    t <- t +
+      xlabel_element
+
+  t
 }
